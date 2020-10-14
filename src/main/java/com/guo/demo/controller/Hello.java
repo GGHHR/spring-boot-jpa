cb4982865c5662d9c766339fc10bf6813ec335fb
guohaoran <1241639333@qq.com>
2020-10-14 16:20:59 +0800

    热更新（需要配合idea设置https://www.jianshu.com/p/c247e5abf27d）

--- src/main/java/com/guo/demo/controller/Hello.java
+++ src/main/java/com/guo/demo/controller/Hello.java
@@ -1,15 +1,18 @@
 package com.guo.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
-@RequestMapping("/")
 public class Hello {
-
     @RequestMapping("/")
     public String hello() {
         return "Hello Spring Boot!";
+    }
+
+    @RequestMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!"+name);
     }
 }
