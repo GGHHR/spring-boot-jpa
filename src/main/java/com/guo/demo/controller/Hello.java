f7ad9ea761809230b844c278f2974ac94312b96d
guohaoran <1241639333@qq.com>
2020-12-22 16:17:12 +0800

    接口

--- src/main/java/com/guo/demo/controller/Hello.java
+++ src/main/java/com/guo/demo/controller/Hello.java
@@ -1,13 +1,18 @@
 package com.guo.demo.controller;
 
+import com.guo.demo.mysql.Greeting;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class Hello {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
     @RequestMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!"+name);
+    public Greeting hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
--- src/test/java/com/guo/demo/DemoApplicationTests.java
+++ src/test/java/com/guo/demo/DemoApplicationTests.java
@@ -1,7 +1,6 @@
 package com.guo.demo;
 
 import com.guo.demo.controller.Hello;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
