740a53e40478a47c9f4a84371edf5ccd278333cf
guohaoran <1241639333@qq.com>
2020-12-22 17:18:16 +0800

    接口

--- src/main/java/com/guo/demo/controller/Hello.java
+++ src/main/java/com/guo/demo/controller/Hello.java
@@ -1,6 +1,8 @@
 package com.guo.demo.controller;
 
 import com.guo.demo.mysql.Greeting;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,13 @@ public class Hello {
 
     @RequestMapping("/hello")
     public Greeting hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+    @GetMapping("/a")
+    public Greeting a(@RequestParam(value = "name", defaultValue = "World") String name) {
+
+//        dbcTemplate.queryForList("select * from city where ID>'2' and ID<'9'");
+
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
