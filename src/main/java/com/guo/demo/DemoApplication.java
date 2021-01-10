13dcc74427ced3767bd23493c5fe96dbeb292460
guo <1241639333@qq.com>
2021-01-10 10:28:35 +0800

    logging

--- src/main/java/com/guo/demo/DemoApplication.java
+++ src/main/java/com/guo/demo/DemoApplication.java
@@ -1,10 +1,19 @@
 package com.guo.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@Slf4j
 @SpringBootApplication
 public class DemoApplication {
+    public DemoApplication() {
+        log.debug("---> debug start! <------");
+        log.info("---> info start! <------");
+        log.warn("---> warn start! <------");
+        log.error("---> error start! <------");
+
+        System.out.println("===> System.out.println <=====");
+    }
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
--- src/main/java/com/guo/demo/controller/Hello.java
+++ src/main/java/com/guo/demo/controller/Hello.java
@@ -1,8 +1,9 @@
 package com.guo.demo.controller;
 
-import com.guo.demo.entity.City;
 import com.guo.demo.repository.CityRepository;
+import org.hibernate.cfg.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,14 +15,23 @@ public class Hello {
     @Autowired
     private CityRepository cityRepo;
 
+    @Value("${devtools.livereload.enabled}")
+    private String autoInject;
+
+    /*获取配置项 yml*/
+    @Value("${spring.datasource.url}")
+    private String url;
+    
+    @GetMapping(path = "url")
+    public String show() {
+        return url;
+    }
+
+
     @GetMapping("/name")
     public @ResponseBody Object  setName(@RequestParam(required = false) Integer id,@RequestParam(required = false) String name) {
 
-        City a = cityRepo.findById(id).get();
-        System.out.println(a);
-        a.setName("2");
-        cityRepo.save(a);
-        return a;
+        return cityRepo.findById(id);
     }
 
 }
