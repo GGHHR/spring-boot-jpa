84bcfc93b8ebef1906dba08cb8e03b8970ee794d
guohaoran <1241639333@qq.com>
2021-01-25 11:23:04 +0800

    1

--- src/main/java/com/guo/demo/config/StocketConfig.java
+++ src/main/java/com/guo/demo/config/StocketConfig.java
@@ -2,7 +2,6 @@ package com.guo.demo.config;
 
 import com.guo.demo.controller.MyWebSocketHandler;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
@@ -12,10 +11,6 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 public class StocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(myHandler(), "wsdemo");
-    }
-
-    public WebSocketHandler myHandler() {
-        return new MyWebSocketHandler();
+        registry.addHandler(new MyWebSocketHandler(), "wsdemo");
     }
 }
