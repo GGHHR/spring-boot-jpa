864761402d83ef7f1ea1200198fc2d5e61aff204
guohaoran <1241639333@qq.com>
2021-02-02 16:13:25 +0800

    1

--- src/main/java/com/guo/demo/controller/Hello.java
+++ src/main/java/com/guo/demo/controller/Hello.java
@@ -1,6 +1,5 @@
 package com.guo.demo.controller;
 
-import com.guo.demo.entity.City;
 import com.guo.demo.repository.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,8 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Date;
 
 
 @RestController
@@ -23,12 +20,11 @@ public class Hello {
     /*获取配置项 yml*/
     @Value("${spring.datasource.url}")
     private String url;
-    
+    /*返回配置项url*/
     @GetMapping(path = "url")
     public String show() {
         return url;
     }
-
 
     @GetMapping("/name")
     public @ResponseBody Object  setName(@RequestParam(required = false) Integer id,@RequestParam(required = false) String name) {
@@ -37,14 +33,6 @@ public class Hello {
     @GetMapping("/ds")
     public @ResponseBody Object  district( @RequestParam(required = false) String district) {
         return cityRepo.findByDistrict(district);
-    }
-    @GetMapping("/save")
-    public @ResponseBody Object  save( @RequestParam(required = false) String name) {
-        City a = new City();
-        a.setName("nihao");
-        a.setCreatAt(new Date());
-        cityRepo.save(a);
-        return  a;
     }
 
 }
