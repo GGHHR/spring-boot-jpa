d4db69c3334cb4dd301995720330e20af726ce8e
guo <1241639333@qq.com>
2021-02-07 10:23:30 +0800

    1

---
+++ src/main/java/com/guo/demo/repository/UserRepo.java
@@ -0,0 +1,10 @@
+package com.guo.demo.repository;
+
+import com.guo.demo.entity.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<City, Integer> {
+
+}
